599e0967bf5f37e50d58bd9e8cb7eab2f423403f
Gustavo Henrique <guazambujinha@gmail.com>
2025-01-26 20:27:23 -0300

    fist commit

---
+++ ProjetoUserdept/src/main/java/com/devsuperior/ProjetoUserdept/ProjetoUserdeptApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ProjetoUserdept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUserdeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUserdeptApplication.class, args);
+	}
+
+}
---
+++ ProjetoUserdept/src/test/java/com/devsuperior/ProjetoUserdept/ProjetoUserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ProjetoUserdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoUserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
