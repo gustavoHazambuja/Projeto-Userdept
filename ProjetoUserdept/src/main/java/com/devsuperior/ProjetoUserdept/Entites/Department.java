672f447efa013b499ba873473eff7e1e496f34e1
Gustavo Henrique <guazambujinha@gmail.com>
2025-01-26 20:49:38 -0300

    user,department add

--- ProjetoUserdept/src/main/java/com/devsuperior/ProjetoUserdept/Entites/Department.java
+++ ProjetoUserdept/src/main/java/com/devsuperior/ProjetoUserdept/Entites/Department.java
@@ -4,33 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
-public class User {
+@Table(name = "tb_department")
+public class Department {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private String email;
 
-    @ManyToOne
-    @JoinColumn(name = "department_id")
-    private Department department;
+    User users;
 
-    public User(){
-
-    }
-
-    public User(Long id, String name, String email, Department department) {
+    public Department(Long id, String name, User users) {
         this.id = id;
         this.name = name;
-        this.email = email;
-        this.department = department;
+        this.users = users;
     }
 
     public Long getId() {
@@ -49,20 +39,12 @@ public class User {
         this.name = name;
     }
 
-    public String getEmail() {
-        return email;
+    public User getUsers() {
+        return users;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
+    public void setUsers(User users) {
+        this.users = users;
     }
 
     @Override
@@ -81,7 +63,7 @@ public class User {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        User other = (User) obj;
+        Department other = (Department) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
